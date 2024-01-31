a097a634df4a62b21f2cf5d29e24861ee4a414f9
Almiron Augusto <79406262+augusto01@users.noreply.github.com>
2024-01-31 10:12:15 -0300

    avance_proyecto_indicesNC

--- optimizaciondeconsultas_indices.sql
+++ optimizaciondeconsultas_indices.sql
@@ -4,10 +4,53 @@ use consorcio
 execute sp_helpindex 'consorcio'
 
 
+--activamos estadisticas de tiempo y de lectura 
+SET STATISTICS TIME ON;
+SET STATISTICS IO ON;
 
 
---creamos un index no clustered para que ordene los registros por apeynom
-CREATE NONCLUSTERED INDEX IX_provincia_descripcion ON provincia(descripcion);
-CREATE NONCLUSTERED INDEX IX_localidad_idprovincia_idlocalidad ON localidad(idprovincia, idlocalidad);
-CREATE NONCLUSTERED INDEX IX_consorcio_idprovincia_idlocalidad_idconsorcio ON consorcio(idprovincia, idlocalidad, idconsorcio);
-CREATE NONCLUSTERED INDEX IX_gasto_idprovincia_idlocalidad_idconsorcio_periodo ON gasto(idprovincia, idlocalidad, idconsorcio, periodo);
+
+
+--CONSULTAMOS 
+SELECT * FROM provincia
+SELECT * FROM localidad
+SELECT * FROM consorcio
+SELECT * FROM gasto
+
+
+--creamos algunos index no clustered 
+CREATE NONCLUSTERED INDEX IDX_provincia_descripcion ON provincia(descripcion);
+CREATE NONCLUSTERED INDEX IDX_localidad_idprovincia_idlocalidad ON localidad(idprovincia, idlocalidad);
+CREATE NONCLUSTERED INDEX IDX_consorcio_idprovincia_idlocalidad_idconsorcio ON consorcio(idprovincia, idlocalidad, idconsorcio);
+CREATE NONCLUSTERED INDEX IDX_gasto_idprovincia_idlocalidad_idconsorcio_periodo ON gasto(idprovincia, idlocalidad, idconsorcio, periodo);
+
+--ejecutamos los select con los indices no clustered
+
+SELECT descripcion
+FROM provincia WITH (INDEX (IDX_provincia_descripcion)) 
+
+
+SELECT descripcion
+FROM localidad WITH (INDEX (IDX_localidad_idprovincia_idlocalidad)) 
+
+--apagar estadisticas
+SET STATISTICS TIME OFF;
+SET STATISTICS IO OFF;
+
+
+
+
+-- Borrar índices en la tabla 'provincia'
+DROP INDEX IF EXISTS IDX_provincia_descripcion ON provincia;
+
+-- Borrar índices en la tabla 'localidad'
+DROP INDEX IF EXISTS IDX_localidad_idprovincia_idlocalidad ON localidad;
+
+-- Borrar índices en la tabla 'consorcio'
+DROP INDEX IF EXISTS IDX_consorcio_idprovincia_idlocalidad_idconsorcio ON consorcio;
+
+-- Borrar índices en la tabla 'gasto'
+DROP INDEX IF EXISTS IDX_gasto_idprovincia_idlocalidad_idconsorcio_periodo ON gasto;
+
+
+
